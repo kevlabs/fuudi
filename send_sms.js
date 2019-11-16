7e52b381140a855a64fa8083953d62fa8e5585f2
james-truong <james.truong@hotmail.com>
2019-11-16 21:06:08 +0000

    added nodetextapp folder for SMS test with socket.io

--- send_sms.js
+++ send_sms.js
@@ -1,8 +1,9 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
-const accountSid = 'AC9041bb97622715c4629da986cdcacb2e';
-const authToken = '660da9a24df77f44b4cc077a101f61ce';
+require('dotenv').load();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
