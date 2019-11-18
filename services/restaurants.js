6704c228bc95af7e3eef8e9db1955bcf60039d2e
james-truong <james.truong@hotmail.com>
2019-11-18 00:31:19 +0000

    @kevlabs

--- services/restaurants.js
+++ services/restaurants.js
@@ -5,6 +5,6 @@ const create = async(db, userId, store) => {
   INSERT INTO resturants (owner_id, name, description, tags, photo_url, open_time, close_time, phone,
     email, street_address, city, post_code, latitude, longitude, wait_minutes, rating, rating_expires_at,
     is_active, is_deleted)
-  VALUES(userId, req.body)
+  VALUES (userId, req.body)
   `)
 }
