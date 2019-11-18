b5211b17cc9a1ea1e29d1ad7ee2e7520f4127d33
james-truong <james.truong@hotmail.com>
2019-11-18 20:58:49 +0000

    added google maps to main page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,5 @@
-// BELOW IS ALL TEST CODE FOR SOCKET
+// BELOW IS FOR SOCKET + GOOGLEMAPS - THESE 2 ARE SEPARATED.
+// 1. SOCKET
 const numberInput = document.getElementById('number'),
       textInput = document.getElementById('msg'),
       scheduleSelect = document.getElementById('schedule'),
@@ -47,3 +48,76 @@ function send() {
   const time = parseInt(scheduleSelect.value, 10);
   getTimeSchedule({ number, text, time });
 }
+
+// 2. GOOGLEMAPS - still need to call backend for restaurant lat + long
+
+<script>
+function initMap() {
+  // Map options
+  var options = {
+    zoom: 8,
+    center: { lat: 42.3601, lng: -71.0589 }
+  }
+
+  // New map
+  var map = new google.maps.Map(document.getElementById('map'), options);
+
+  // Listen for click on map
+  google.maps.event.addListener(map, 'click', function (event) {
+    // Add marker
+    addMarker({ coords: event.latLng });
+  });
+
+
+  // Array of markers
+  var markers = [
+    {
+      coords: { lat: 43.644367, lng: -79.400774 },
+      content: '<h1>Starbucks</h1>'
+    },
+    {
+      coords: { lat: 43.644284, lng: -79.403030 },
+      content: '<h1>Freshii</h1>'
+    },
+    {
+      coords: { lat: 43.644090, lng: -79.403151 },
+      content: '<h1>McDonalds</h1>'
+    }
+  ];
+
+  // Loop through markers
+  for (var i = 0; i < markers.length; i++) {
+    // Add marker
+    addMarker(markers[i]);
+  }
+
+  // Add Marker Function
+  function addMarker(props) {
+    var marker = new google.maps.Marker({
+      position: props.coords,
+      map: map,
+      //icon:props.iconImage
+    });
+
+    // Check for customicon
+    if (props.iconImage) {
+      // Set icon image
+      marker.setIcon(props.iconImage);
+    }
+
+    // Check content
+    if (props.content) {
+      var infoWindow = new google.maps.InfoWindow({
+        content: props.content
+      });
+
+      marker.addListener('click', function () {
+        infoWindow.open(map, marker);
+      });
+    }
+  }
+}
+</script>
+<script async defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA372PcuJQKYrKADjhHTLLDVcfsoCzF-4M&callback=initMap">
+</script>
