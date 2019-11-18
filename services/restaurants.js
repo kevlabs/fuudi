38b9b0e0386eb7a217234858773b482a20d4890f
james-truong <james.truong@hotmail.com>
2019-11-18 00:23:41 +0000

    add socket to work, split up main.js

---
+++ services/restaurants.js
@@ -0,0 +1,10 @@
+const { stringToInteger } = require('../lib/utils');
+
+const create = async(db, userId, store) => {
+  let newStore = await query(`
+  INSERT INTO resturants (owner_id, name, description, tags, photo_url, open_time, close_time, phone,
+    email, street_address, city, post_code, latitude, longitude, wait_minutes, rating, rating_expires_at,
+    is_active, is_deleted)
+  VALUES(userId, req.body)
+  `)
+}
