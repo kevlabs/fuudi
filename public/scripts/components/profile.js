5113b24a3330738c6e18c39bac80137bb9cd30b6
Matthew Wardle <mwardle.22@gmail.com>
2019-11-19 20:46:23 +0000

    Edit components for consistency

--- public/scripts/components/profile.js
+++ public/scripts/components/profile.js
@@ -2,24 +2,22 @@ const escape = require('../utils');
 
 // TODO: convert to use render()?
 
-const profile = new ViewComponent(
-  $(
-    `
-       <div class="content">
-        <div class="profile-info">
-          <div class="user-img">
-            <img src="${escape(user.photoUrl)}"
-              class="profile-picture img-responsive rounded-circle">
+class Profile extends ViewComponent {
+  render(user) {
+
+    return $(`
+         <div class="content">
+          <div class="profile-info">
+            <div class="user-img">
+              <img src="${user.photoUrl}"
+                class="profile-picture img-responsive rounded-circle">
+            </div>
+            <p class="profile-name">${user.username}</p>
+            <p class="user-details">Email: ${user.email}</p>
+            <p class="user-details">Phone: ${user.phone}</p>
           </div>
-          <p class="profile-name">${escape(user.username)}</p>
-          <p class="user-details">Email: ${escape(user.email)}</p>
-          <p class="user-details">Phone: ${escape(user.phone)}</p>
-        </div>
-
-        <div class="listing-container">
-
-        </div>
-
-      `
-  )
-);
+          <div class="listing-container">
+          </div>
+  `)
+  }
+};
