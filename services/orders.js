daa5a7e270e2d879e4130d1202ffb62ae59b3aee
Kevin Laburte <kevin.laburte@gmail.com>
2019-11-22 00:37:36 -0500

    Fix order status bug

--- services/orders.js
+++ services/orders.js
@@ -36,6 +36,11 @@ const setGetFilters = (options) => {
     filters.push(`o.user_id = $${params.length}`);
   }
 
+  if (options.ownerId) {
+    params.push(options.ownerId);
+    filters.push(`r.owner_id = $${params.length}`);
+  }
+
   if (options.restaurantId) {
     const id = stringToInteger(options.restaurantId, (int) => int > 0, true);
     params.push(id);
@@ -194,8 +199,8 @@ const notify = async (db, textMessages, orderId) => {
     const userMessage = `Hey, this is Fuudi. You have an order in progress with ${name} (current status: ${status.toLowerCase()}). It should be ready in about ${waitMinutes} minutes (${toTimeString(estimatedFulfilled)}). Stay fresh, always.`;
     const ownerMessage = `To the folks at ${name}. Greetings from Fuudi. An order is in progress. Current status: ${status.toLowerCase()}. Estimated completion time: ${toTimeString(estimatedFulfilled)}. Connect to your Fuudi terminal to manage your orders.`;
 
-    await textMessages.send(userPhone, userMessage);
-    await textMessages.send(ownerPhone, ownerMessage);
+    // await textMessages.send(userPhone, userMessage);
+    // await textMessages.send(ownerPhone, ownerMessage);
 
   } catch (err) {
     throw Error(`Could not notify all parties to the order. Error: ${err.message}`);
